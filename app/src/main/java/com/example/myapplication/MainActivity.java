cbcfbe120f1ce3faa87f7ae4d9214eb6377fc85a
rafaelvergi <169174493+rafaelvergi@users.noreply.github.com>
2025-05-06 14:52:51 -0300

    Commit do Rafael

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,84 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editCapital, editJuros, editTaxa, editPrazo;
+    Spinner spinner;
+    Button btnCalcular;
+    String criterioSelecionado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        spinner = findViewById(R.id.spinner);
+        editCapital = findViewById(R.id.editCapital);
+        editJuros = findViewById(R.id.editJuros);
+        editTaxa = findViewById(R.id.editTaxa);
+        editPrazo = findViewById(R.id.editPrazo);
+        btnCalcular = findViewById(R.id.btnCalcular);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.opcoes_calculo, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                criterioSelecionado = parent.getItemAtPosition(position).toString();
+                habilitarCampos(criterioSelecionado);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }
+        });
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcular();
+            }
         });
     }
-}
+
+    private void habilitarCampos(String criterio) {
+        editCapital.setEnabled(true);
+        editJuros.setEnabled(true);
+        editTaxa.setEnabled(true);
+        editPrazo.setEnabled(true);
+
+        switch (criterio) {
+            case "Capital":
+                editCapital.setEnabled(false);
+                editCapital.setText("");
+                break;
+            case "Juros":
+                editJuros.setEnabled(false);
+                editJuros.setText("");
+                break;
+            case "Taxa":
+                editTaxa.setEnabled(false);
+                editTaxa.setText("");
+                break;
+            case "Prazo":
+                editPrazo.setEnabled(false);
+                editPrazo.setText("");
+                break;
+        }
+    }
+
+
+
